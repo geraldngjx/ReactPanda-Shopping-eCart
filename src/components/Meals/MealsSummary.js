9215a8bc74d4d90c253e7733e9dad8289ca71144
Gerald Ng <geraldngjx@gmail.com>
2022-05-27 00:29:24 +0800

    initial commit

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,17 @@
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Fast Delivery - Just like how responsive React websites are</h2>
+      <p>
+        Choose your favorite food of choice and expect it at your doorsteps within minutes!
+      </p>
+      <p>
+        Wide Variety of Unique Food - As many as the numourous functionalities that React has!
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
